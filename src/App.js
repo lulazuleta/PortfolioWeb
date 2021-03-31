d068dadf67fd1c44f7e9c2fa564819ac6fdf0baf
lulazuleta <lucia.lzuleta@gmail.com>
2021-03-30 21:07:59 -0300

    Segunda entrega curso react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="navbar">
+  <a class="inicio" href="#inicio">Inicio</a>
+  <a href="#Productos">Productos</a>
+  <a href="#contacto">Contactanos</a>
+  <a href="#nosotros">Nosotros</a>
+</div>
   );
 }
 
