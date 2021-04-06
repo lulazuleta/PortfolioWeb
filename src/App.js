35edbb7b359a3cb537c5769dec99100016b9e1de
lulazuleta <lucia.lzuleta@gmail.com>
2021-04-05 22:29:08 -0300

    Tercer entrega curso react

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div class="navbar">
-  <a class="inicio" href="#inicio">Inicio</a>
-  <a href="#Productos">Productos</a>
-  <a href="#contacto">Contactanos</a>
-  <a href="#nosotros">Nosotros</a>
-</div>
+    
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto"><a href="index.html">Proyecto REACT</a></h1>
+      
+      
+          <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Inicio</a></li>
+          <li><a href="#productos">Productos</a></li>
+          <li><a href="#nosotros">Nosotros</a></li> 
+          <li><a href="#contacto">Contacto</a></li>
+
+        </ul>
+      </nav>
+
+      
+
+    </div>
+  
   );
 }
 
